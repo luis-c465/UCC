bce8e4e4efba4a368933cc3b01c81744b5412078
Luis Canada <canadaluis5@gmail.com>
2023-04-16 11:06:31 -0400

    Update electrical

--- src/main/java/luisc/ucc/explore/Electrical.java
+++ src/main/java/luisc/ucc/explore/Electrical.java
@@ -10,42 +10,43 @@ public class Electrical extends Exploration {
   }
 
   @Override
-  protected void _update() {
-    if (isClicked("Go back outside")) {
-      setExploration(new ElectricalHallway(a));
-    }
-  }
+  protected void _update() {}
 
   @Override
   protected void _setup() {
-    bg = p.loadImage("options/electrical-1.png");
+    bg = loadGif("options/electrical.gif");
 
     clickables =
       new Clickable[] {
-        new ExplorationBtn(a, "Go back outside", 800, 800),
         new Evidence(
           a,
-          "Wifi",
-          260,
-          352,
+          "wifi",
+          254,
+          192,
           "MC: The communications panel is malfunctioning, thats a bit sus"
         ),
         new Evidence(
           a,
-          "wires",
-          374,
-          517,
+          "electrical-wires",
+          367,
+          525,
           "MC: Who did a little trolling here"
         ),
-        new Evidence(a, "wires", 813, 628, "MC: Why are the wires cut?"),
+        new Evidence(a, "wires", 818, 764, "MC: Why are the wires cut?"),
         new Evidence(
           a,
-          "vent",
-          244,
-          539,
+          "electrical-grease",
+          245,
+          568,
           "MC: Grease on the vents, interesting"
         ),
-        new Evidence(a, "electricity", 950, 450, "MC: Why is this open?"),
+        new Evidence(
+          a,
+          "electrical-cabinet",
+          939,
+          373,
+          "MC: Why is this open?"
+        ),
       };
   }
 }
